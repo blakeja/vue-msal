d612991c7479b6d2f7c4702606b88c72f4f24c78
James Blake <james.blake@rackspace.com>
2020-03-20 16:20:58 -0500

    style updates

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -5,23 +5,27 @@ import { mixin } from "./mixin";
 export const msalMixin = mixin;
 
 export default class msalPlugin {
-    static install(Vue: any, options: Options): void {
-        Vue.prototype.$msal = new msalPlugin(options, Vue);
+  static install(Vue: any, options: Options): void {
+    Vue.prototype.$msal = new msalPlugin(options, Vue);
+  }
+
+  constructor(options: Options, Vue: any = undefined) {
+    const msal = new MSAL(options);
+
+    if (Vue && options.framework && options.framework.globalMixin) {
+      Vue.mixin(mixin);
     }
-    constructor(options: Options, Vue: any = undefined) {
-        const msal = new MSAL(options);
-        if (Vue && options.framework && options.framework.globalMixin) {
-            Vue.mixin(mixin);
-        }
-        const exposed: MSALBasic = {
-            data: msal.data,
-            signIn() { msal.signIn(); },
-            async signOut() { await msal.signOut(); },
-            isAuthenticated() { return msal.isAuthenticated(); },
-            async acquireToken(request) { return await msal.acquireToken(request); },
-            async msGraph(endpoints, batchUrl) { return await msal.msGraph(endpoints, batchUrl) },
-            saveCustomData(key: string, data: any) { msal.saveCustomData(key, data); }
-        };
-        return exposed;
-    }
+
+    const exposed: MSALBasic = {
+      data: msal.data,
+      signIn() { msal.signIn(); },
+      async signOut() { await msal.signOut(); },
+      isAuthenticated() { return msal.isAuthenticated(); },
+      async acquireToken(request) { return await msal.acquireToken(request); },
+      async msGraph(endpoints, batchUrl) { return await msal.msGraph(endpoints, batchUrl) },
+      saveCustomData(key: string, data: any) { msal.saveCustomData(key, data); }
+    };
+    
+    return exposed;
+  }
 }
--- lib/src/types.ts
+++ lib/src/types.ts
@@ -6,73 +6,77 @@ export type AuthError = msal.AuthError;
 export type AuthResponse = msal.AuthResponse;
 
 export type Auth = {
-    clientId: string,
-    tenantId?: string,
-    tenantName?: string,
-    validateAuthority?: boolean;
-    redirectUri?: string | (() => string);
-    postLogoutRedirectUri?: string | (() => string);
-    navigateToLoginRequestUrl?: boolean;
-    requireAuthOnInitialize?: boolean,
-    autoRefreshToken?: boolean,
-    onAuthentication: (ctx: object, error: AuthError, response: AuthResponse) => any,
-    onToken: (ctx: object, error: AuthError | null, response: AuthResponse | null) => any,
-    beforeSignOut: (ctx: object) => any
+  clientId: string,
+  tenantId?: string,
+  tenantName?: string,
+  validateAuthority?: boolean;
+  redirectUri?: string | (() => string);
+  postLogoutRedirectUri?: string | (() => string);
+  navigateToLoginRequestUrl?: boolean;
+  requireAuthOnInitialize?: boolean,
+  autoRefreshToken?: boolean,
+  onAuthentication: (ctx: object, error: AuthError, response: AuthResponse) => any,
+  onToken: (ctx: object, error: AuthError | null, response: AuthResponse | null) => any,
+  beforeSignOut: (ctx: object) => any
 }
 
 export type Request = {
-    scopes?: string[]
+  scopes?: string[]
 }
 
 export type GraphDetailedObject = AxiosRequestConfig & {
-    url: string,
-    id?: string
+  url: string,
+  id?: string
 }
+
 export type GraphEndpoints = string | GraphDetailedObject | Array<string | GraphDetailedObject>
+
 export type Graph = {
-    callAfterInit?: boolean,
-    baseUrl?: string,
-    endpoints?: { [id: string]: string | GraphDetailedObject },
-    onResponse?: (ctx: object, response: object) => any
+  callAfterInit?: boolean,
+  baseUrl?: string,
+  endpoints?: { [id: string]: string | GraphDetailedObject },
+  onResponse?: (ctx: object, response: object) => any
 }
 
 export type CacheOptions = conf.CacheOptions;
+
 export type SystemOptions = conf.SystemOptions;
+
 export type FrameworkOptions = {
-    globalMixin?: boolean
+  globalMixin?: boolean
 }
 
 export type Options = {
-    auth: Auth,
-    request?: Request,
-    graph?: Graph,
-    cache?: CacheOptions,
-    system?: SystemOptions,
-    framework?: FrameworkOptions
+  auth: Auth,
+  request?: Request,
+  graph?: Graph,
+  cache?: CacheOptions,
+  system?: SystemOptions,
+  framework?: FrameworkOptions
 }
 
 export type DataObject = {
-    isAuthenticated: boolean,
-    accessToken: string,
-    user: object,
-    graph: object,
-    custom: object
+  isAuthenticated: boolean,
+  accessToken: string,
+  user: object,
+  graph: object,
+  custom: object
 }
 
 export type CallbackQueueObject = {
-    id: string,
-    callback: string,
-    arguments: any[]
+  id: string,
+  callback: string,
+  arguments: any[]
 }
 
 export interface MSALBasic {
-    data: DataObject,
-    signIn: () => void,
-    signOut: () => Promise<any> | void,
-    isAuthenticated: () => boolean,
-    acquireToken: (request: Request) => Promise<string | boolean>,
-    msGraph: (endpoints:  GraphEndpoints, batchUrl: string | undefined) => Promise<object>,
-    saveCustomData: (key: string, data: any) => void
+  data: DataObject,
+  signIn: () => void,
+  signOut: () => Promise<any> | void,
+  isAuthenticated: () => boolean,
+  acquireToken: (request: Request) => Promise<string | boolean>,
+  msGraph: (endpoints:  GraphEndpoints, batchUrl: string | undefined) => Promise<object>,
+  saveCustomData: (key: string, data: any) => void
 }
 
 export type CategorizedGraphRequests = { singleRequests: GraphDetailedObject[], batchRequests: { [id:string]: GraphDetailedObject[] } }
